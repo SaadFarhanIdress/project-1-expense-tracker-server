78a530f20ca54489730af27954be39e51ab4864e
SaadFarhanIdress <sunehrafarhan123@gmail.com>
2021-06-24 11:58:07 +0500

    Created a simple server

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const colors = require('colors');
+const morgan = require('morgan');
+const app = express();
+
+dotenv.config({ path: './config.env' })
+
+
+
+app.listen(port, (err) => {
+    if (err) {
+        console.log("Error", err)
+        return;
+    }
+    console.log(`Server started on ${port} on ${new Date().toDateString()}....`.yellow.bold)
+})
