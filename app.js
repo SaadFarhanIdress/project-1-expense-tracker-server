89dc10e6b5b279ac1fd8d09eaa4cc5fe54966c82
SaadFarhanIdress <sunehrafarhan123@gmail.com>
2021-06-24 12:00:37 +0500

    Added needed packages

--- app.js
+++ app.js
@@ -3,10 +3,15 @@ const dotenv = require('dotenv');
 const colors = require('colors');
 const morgan = require('morgan');
 const app = express();
+const port = process.env.PORT || 5000;
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const { connectionWithDB } = require('./helpers/connectToDB');
 
 dotenv.config({ path: './config.env' })
 
+app.use(bodyParser.json())
+app.use(cors())
 
 
 app.listen(port, (err) => {
