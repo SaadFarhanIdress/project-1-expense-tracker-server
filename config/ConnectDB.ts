ee8c8f7928796fb0652b18b8b1a9f2169937aa1c
SaadFarhanIdress <saadfarhan347@gmail.com>
2022-07-10 03:07:58 +0500

    Fix env problems

--- config/ConnectDB.ts
+++ config/ConnectDB.ts
@@ -1,7 +1,12 @@
 import { connect, connection } from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config({ path: __dirname + '/config.env' })
+
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_SERVER}.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
 export default function ConnectDB() {
-    connect(process.env.MONGODB_URI, {
+    connect(uri, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
     });
